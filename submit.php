b22c7a0ee3af12a1ab6045c0d326ae3ded65ddb8
dhierholzer <dhierholzer@gmail.com>
2015-05-13 14:40:44 -0400

    Made Changers Trying To Use JSON

--- submit.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$pizzacrustinput = $_POST["data1"];
-$_SESSION['pizzacrustinput'] = $pizzacrustinput;
-echo "The Type Of Crust Is " . $_SESSION["pizzacrustinput"] . ".";
-
-?>
--- toppingtypes.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$selectedarray = $_POST["selectedarray"];
-
-$_SESSION['selectedarray'] = $selectedarray;
-echo "." . $_SESSION["selectedarray"] . ".";
-?>
